766bdcea7681a0fa5638e2651fb2fd39599c690a
michael-zhao <michael.xin.zhao@gmail.com>
2019-10-16 13:09:48 -0400

    change target to seattle 2013 (super bowl year)

--- scraper.py
+++ scraper.py
@@ -2,10 +2,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 
-url = "https://www.pro-football-reference.com/teams/sea/2019.htm"
+# reads data from Seattle's 2013 page
+url = "https://www.pro-football-reference.com/teams/sea/2013/gamelog/"
 page = urlopen(url).read()
-print(page)
 
-# soup = BeautifulSoup(page)
-  # count = 0
-  # table = soup.find()
+soup = BeautifulSoup(page)
+count = 0
+table = soup.find("tbody")
+pre_df = dict()
+features = {'opp', ''}
